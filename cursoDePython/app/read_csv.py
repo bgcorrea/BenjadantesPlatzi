f6d804fb754f731d51b47ddfb95a12dcaec8c7b5
bgcorrea <bgcorrea@uc.cl>
2023-05-20 22:36:28 -0400

    nuevo commit

---
+++ cursoDePython/app/read_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+
+def read_csv(path):
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print('***' * 5)
+            print(row)
+            
+if __name__ == '__main__':
+    read_csv('./app/data.csv')
